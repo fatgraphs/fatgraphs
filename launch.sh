d556acbc70dca2797b86d26a6a9ed24661a130f8
segatcarlo95@gmail.com <Rospo1Rospo1>
2021-03-18 14:27:07 +0100

    added tiles creation to launch script

--- launch.sh
+++ launch.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# to store spwned processes IDs
 declare -a pids
 cwd=$(pwd)
 
+# on exit kill the spwned processes
 cleanup() {
+  echo "Killing server and client"
     for pid in "${pids[@]}"; do
         kill -9 "$pid"
     done
 }
 trap "cleanup" SIGINT SIGTERM
 
+#generate tiles
+PYTHONPATH=$(pwd) python be/tile_creator/src/main.py
+
 # run server
+cd $cwd
 cd be/tile_server && export FLASK_APP=server.py
 flask run &
 pids+=("$!")
