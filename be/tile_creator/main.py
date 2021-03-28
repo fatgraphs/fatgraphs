2f0ef0a0ce5727a6e5f100fc3a37f4150684d1f8
segatcarlo95@gmail.com <Rospo1Rospo1>
2021-03-28 18:01:50 +0200

    main scriot of tile creation loads the mock labelled graph. The purpose is to have a graph that has nodes that have exchange labels

--- be/tile_creator/main.py
+++ be/tile_creator/main.py
@@ -1,9 +1,10 @@
-from be.configuration import MEDIUM_GRAPH_RAW_PATH, SMALL_GRAPH_RAW_PATH, CONFIGURATIONS
+from be.configuration import MEDIUM_GRAPH_RAW_PATH, SMALL_GRAPH_RAW_PATH, CONFIGURATIONS, MOCK_LABELLED_RAW_PATH, \
+    LABELS_PATH, METADATA_PATH
 from be.tile_creator.src.graph.gt_token_graph import GraphToolTokenGraph
 from be.tile_creator.src.graph.token_graph import TokenGraph
 from be.tile_creator.src.render.renderer import GraphRenderer
 
-medium_graph = TokenGraph(MEDIUM_GRAPH_RAW_PATH, {'dtype': {'amount': object}})
+medium_graph = TokenGraph(MOCK_LABELLED_RAW_PATH, {'dtype': {'amount': object}}, LABELS_PATH)
 gt_graph = GraphToolTokenGraph(medium_graph)
 renderer = GraphRenderer(gt_graph)
 renderer.render_tiles(CONFIGURATIONS['zoom_levels'])
