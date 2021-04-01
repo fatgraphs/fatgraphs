fbf8383b7d99f779e2a50bdb66409c05bd292b3a
segatcarlo95@gmail.com <Rospo1Rospo1>
2021-04-01 13:55:28 +0200

    removed tile source from BE configurations as it was assuming only one tile source but we are now showing multiple graphs (ie we have many different sources)

--- be/configuration.py
+++ be/configuration.py
@@ -4,7 +4,6 @@ import json
 this_file_dir = os.path.dirname(os.path.realpath(__file__))
 data_folder = os.path.join(this_file_dir, 'data')
 
-TILE_SOURCE = os.path.join(this_file_dir, 'tiles')
 MEDIUM_GRAPH_RAW_PATH = os.path.join(data_folder, 'medium.csv')
 SMALL_GRAPH_RAW_PATH = os.path.join(data_folder, 'small.csv')
 MOCK_LABELLED_RAW_PATH = os.path.join(data_folder, 'mock_net_labelled.csv')
