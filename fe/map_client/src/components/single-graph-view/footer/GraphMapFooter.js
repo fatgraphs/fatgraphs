b58bbd955822a37fce26671b92ec90c9997a714c
segatcarlo95@gmail.com <Rospo1Rospo1>
2021-04-03 16:20:01 +0200

    client: added gallery components, refactored 2

---
+++ fe/map_client/src/components/single-graph-view/footer/GraphMapFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class GraphMapFooter extends React.Component {
+    render() {
+        return <footer>
+            <p>TokenGallery footer</p>
+            <p>Author: Friedhelm Victor</p>
+            </footer>;
+    }
+}
+
+export default GraphMapFooter
