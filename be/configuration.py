63a8a273f6ae93a14a4a148f7107533ea5374dce
segatcarlo95@gmail.com <Rospo1Rospo1>
2021-04-08 16:10:47 +0200

    added large graph path to constants

--- be/configuration.py
+++ be/configuration.py
@@ -4,6 +4,7 @@ import json
 this_file_dir = os.path.dirname(os.path.realpath(__file__))
 data_folder = os.path.join(this_file_dir, 'data')
 
+LARGE_GRAPH_RAW_PATH = os.path.join(data_folder, 'large.csv')
 MEDIUM_GRAPH_RAW_PATH = os.path.join(data_folder, 'medium.csv')
 SMALL_GRAPH_RAW_PATH = os.path.join(data_folder, 'small.csv')
 MOCK_LABELLED_RAW_PATH = os.path.join(data_folder, 'mock_net_labelled.csv')
