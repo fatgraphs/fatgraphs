4af18c03ac164a5cec1a072abce33bd747712690
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-05-10 08:23:41 +0200

    moved definition of test source to constants.py

--- be/tile_creator/test/constants.py
+++ be/tile_creator/test/constants.py
@@ -1,6 +1,7 @@
 import os
 
-TEST_DATA = os.path.join("../data", "test_graph.csv")
+this_file_path = os.path.dirname(os.path.realpath(__file__))
+TEST_DATA = os.path.join(this_file_path, "data", "test_graph.csv")
 RAW_EDGES = 278
 PREPROCESSED_EDGES = 252
 UNIQUE_ADDRESSES = 197
