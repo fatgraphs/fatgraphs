7bda0eed4e2b5a94a6c03d9c67778f07475b4e8a
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-05-17 11:48:32 +0200

    test mechanism for push & test

--- be/tile_creator/src/layout/visual_layout.py
+++ be/tile_creator/src/layout/visual_layout.py
@@ -24,7 +24,7 @@ def convert_graph_coordinate_to_map(source_x, source_y, target_x, target_y,
 class VisualLayout:
 
     default_force_atlas_2_options = {'max_iter': 500,
-                                     'strong_gravity_mode': True,
+                                     'strong_gravity_mode': False,
                                      'barnes_hut_theta': 1.2,
                                      'outbound_attraction_distribution': False,
                                      'gravity': 1,
