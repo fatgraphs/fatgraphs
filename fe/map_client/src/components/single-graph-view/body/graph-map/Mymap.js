f02cb41d28ae3615bc496a856a5ea349d4614029
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-05-27 09:07:15 +0200

    util function to convert a point in map coordinates to graph coordinates (needed for the proximity click mechanism)

--- fe/map_client/src/components/single-graph-view/body/graph-map/Mymap.js
+++ fe/map_client/src/components/single-graph-view/body/graph-map/Mymap.js
@@ -141,16 +141,25 @@ export default Mymap;
  * @param tile_size tile size used when generating the graph tiles
  */
 function convert_graph_coordinate_to_map(graph_coordinate, g_min, g_max, tile_size) {
-    console.log("graph_coordinate" + graph_coordinate)
+    // console.log("graph_coordinate" + graph_coordinate)
     // console.log(g_min)
     // console.log(g_max)
     let graph_side = g_max - g_min
-    console.log("graph sixe: " + graph_side)
-    let map_x = (graph_coordinate[0] + Math.abs(g_min)) * tile_size / graph_side
-    let map_y = (graph_coordinate[1] + Math.abs(g_min)) * tile_size / graph_side
-    // console.log(">>>>>>>>>>>>>>")
-    console.log([-map_y, map_x])
-    return [-map_y, map_x]
+    // console.log("graph sixe: " + graph_side)
+    let map_x = -1 * (graph_coordinate[1] + Math.abs(g_min)) * tile_size / graph_side
+    let map_y = (graph_coordinate[0] + Math.abs(g_min)) * tile_size / graph_side
+    // console.log([-map_y, map_x])
+    return [map_x, map_y]
+
+}
+
+function convert_map_coordinate_to_graph(map_coordinate, g_min, g_max, tile_size){
+    let graph_side = g_max - g_min
+    // console.log("graph sixe: " + graph_side)
+    let graph_x = (map_coordinate[1] * graph_side / tile_size) - Math.abs(g_min)
+    let graph_y = -(map_coordinate[0] * graph_side / tile_size) - Math.abs(g_min)
+    // console.log([-map_y, map_x])
+    return [graph_x, graph_y]
 }
 
 function parseTuple(t) {
