9891fcc2cf902d5b7ce449293b4977bd65c357e2
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-06-09 14:59:06 +0200

    BE refactoring The API calls relative to a signle graph have been  moved into their own Blueprint (flask concept, similar to module). All methods of the signle_graph_blueprint automatically execute a check function that checks if the parameter graph_name has been supplied and if the db contains the required tables for it.

--- be/server/server.py
+++ be/server/server.py
@@ -1,12 +1,12 @@
 import os
-from flask import jsonify, send_from_directory
-from flask import Flask, send_file, safe_join
-from werkzeug.routing import IntegerConverter, FloatConverter
-from flask_cors import CORS, cross_origin
-from be.configuration import CONFIGURATIONS, VERTEX_TABLE_NAME
-import pandas as pd
 
-from be.persistency.nice_abstraction import singletonNiceAbstraction
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from werkzeug.routing import IntegerConverter, FloatConverter
+
+from be.configuration import CONFIGURATIONS
+from be.server.single_graph_api import single_graph_api, check_graph_exists
 
 
 class SignedIntConverter(IntegerConverter):
@@ -15,9 +15,14 @@ class SignedIntConverter(IntegerConverter):
 
 # before routes are registered
 app = Flask(__name__)
-cors = CORS(app)
 app.url_map.converters['signed_int'] = SignedIntConverter
 app.url_map.converters['float'] = FloatConverter
+cors = CORS(app)
+
+app.register_blueprint(single_graph_api)
+app.before_request_funcs = {
+    'single_graph_api': [check_graph_exists]
+}
 
 
 @app.route('/', defaults={'path': ''})
@@ -32,67 +37,8 @@ def get_available_graphs():
     return jsonify(graph_names)
 
 
-@app.route(
-    CONFIGURATIONS['endpoints']['tile'] + '/<string:graph_name>/<signed_int:z>/<signed_int:x>/<signed_int:y>.png')
-def get_tile(graph_name, z, x, y):
-    # print("recevied: " + str(z) + " " + str(x) + " " + str(y))
-
-    tile_name = 'z_' + str(z) + 'x_' + str(x) + 'y_' + str(y) + '.png'
-    source = os.path.join(CONFIGURATIONS['graphs_home'], graph_name)
-    join = safe_join(source, tile_name)
-    if os.path.isfile(os.path.join(source, tile_name)):
-        return send_from_directory("../..", join, mimetype='image/jpeg')
-    else:
-        return 'Not present'
-
-
-@app.route(CONFIGURATIONS['endpoints']['vertices_metadata'] + '/<graph_name>')
-def get_nodes_metadata(graph_name):
-    source = os.path.join(CONFIGURATIONS['graphs_home'], graph_name, CONFIGURATIONS['vertices_metadata_file_name'])
-    if os.path.exists(source):
-        csv = pd.read_csv(source)
-    else:
-        raise Exception("The metadata.csv file fro the graph is missing")
-    if csv.empty:
-        return {}
-    csv['pos'] = csv.apply(lambda row: (round(row['x'], 2), round(row['y'], 2)), axis=1)
-    csv = csv.drop(columns=['x', 'y'])
-    response = {}
-    for i, r in csv.iterrows():
-        response[str(r['pos'])] = [str(r['label']), str(r['address'])]
-    return response
-
-
-@app.route(CONFIGURATIONS['endpoints']['graph_metadata'] + '/<graph_name>')
-def get_graph_metadata(graph_name):
-    source = os.path.join(CONFIGURATIONS['graphs_home'], graph_name, CONFIGURATIONS['graph_metadata_file_name'])
-    if os.path.exists(source):
-        csv = pd.read_csv(source)
-    else:
-        raise Exception("The vertices metadata file is missing")
-    metadata_dictionary = csv.to_dict(orient='records')[0]
-    metadata_dictionary['labels'] = str(metadata_dictionary['labels'])
-    return metadata_dictionary
-
-
-@app.route(CONFIGURATIONS['endpoints']['edge_distributions'] + '/<graph_name>/<zoom_level>')
-def get_distributions(graph_name, zoom_level):
-    path_join = os.path.join(CONFIGURATIONS['graphs_home'], graph_name)
-    distribution_file_names = list(filter(lambda x: 'distribution' in x, os.listdir(path_join)))
-    distribution_file_name = list(filter(lambda x: str(zoom_level) in x, distribution_file_names))[0]
-
-    file = os.path.join(CONFIGURATIONS['graphs_home'], graph_name, distribution_file_name)
-    return send_from_directory("../..", file, mimetype='image/jpeg')
-
-
-@app.route(
-    CONFIGURATIONS['endpoints']['proximity_click'] + '/<graph_name>/<float(signed=True):x>/<float(signed=True):y>')
-def get_closest_vertex(graph_name, x, y):
-    # return str(x) + str(y) + str(graph_name)
-    db_query_result = singletonNiceAbstraction.get_closest_point(x, y, VERTEX_TABLE_NAME(graph_name))
-    vertex_id = db_query_result[0]
-    eth = singletonNiceAbstraction.get_eth(graph_name, vertex_id)
-    closest_point = db_query_result[1].split('(')[-1].split(')')[0].split(' ')
-    return {'eth': eth,
-            'x': closest_point[0],
-            'y': closest_point[1]}
+# TODO: for development return the exception to the client, remove in production
+@app.errorhandler(Exception)
+def server_error(err):
+    app.logger.exception(err)
+    return str(err)
---
+++ be/server/single_graph_api.py
@@ -0,0 +1,75 @@
+import os
+
+from flask import Blueprint, request, safe_join, send_from_directory
+from werkzeug.routing import IntegerConverter
+from be.configuration import CONFIGURATIONS, VERTEX_TABLE_NAME
+from be.persistency.nice_abstraction import singletonNiceAbstraction
+from be.utils.utils import wkt_to_x_y_list
+
+
+class SignedIntConverter(IntegerConverter):
+    regex = r'-?\d+'
+
+
+single_graph_api = Blueprint('single_graph_api', __name__)
+
+def check_graph_exists():
+    graph_name = request.view_args['graph_name']
+    if not singletonNiceAbstraction.is_graph_in_db(graph_name):
+        raise Exception(f'It looks like the graph: {graph_name} was not correctly saved in the db.\n'
+                        f'some or all db tables are missing, or have the wrong name.')
+
+
+@single_graph_api.route(CONFIGURATIONS['endpoints']['graph_metadata'] + '/<graph_name>')
+def get_graph_metadata(graph_name):
+    metadata_frame = singletonNiceAbstraction.get_graph_metadata(graph_name)
+    metadata_dictionary = metadata_frame.to_dict(orient='records')[0]
+    return metadata_dictionary
+
+
+@single_graph_api.route(CONFIGURATIONS['endpoints']['vertices_metadata'] + '/<graph_name>')
+def get_nodes_metadata(graph_name):
+    ids = singletonNiceAbstraction.get_labelled_vertices(graph_name)
+    response = {}
+    for i, r in ids.iterrows():
+        pos = wkt_to_x_y_list(r['st_astext'])
+        response[str(tuple(pos))] = [str(r['label']), str(r['eth'])]
+    return response
+
+
+@single_graph_api.route(
+    CONFIGURATIONS['endpoints']['tile'] + '/<string:graph_name>/<signed_int:z>/<signed_int:x>/<signed_int:y>.png')
+def get_tile(graph_name, z, x, y):
+    # TODO move the imgs in the DB
+    # print("recevied: " + str(z) + " " + str(x) + " " + str(y))
+
+    tile_name = 'z_' + str(z) + 'x_' + str(x) + 'y_' + str(y) + '.png'
+    source = os.path.join(CONFIGURATIONS['graphs_home'], graph_name)
+    join = safe_join(source, tile_name)
+    if os.path.isfile(os.path.join(source, tile_name)):
+        return send_from_directory("../..", join, mimetype='image/jpeg')
+    else:
+        return 'Not present'
+
+
+@single_graph_api.route(CONFIGURATIONS['endpoints']['edge_distributions'] + '/<graph_name>/<zoom_level>')
+def get_distributions(graph_name, zoom_level):
+    # TODO move the imgs in the DB
+    path_join = os.path.join(CONFIGURATIONS['graphs_home'], graph_name)
+    distribution_file_names = list(filter(lambda x: 'distribution' in x, os.listdir(path_join)))
+    distribution_file_name = list(filter(lambda x: str(zoom_level) in x, distribution_file_names))[0]
+
+    file = os.path.join(CONFIGURATIONS['graphs_home'], graph_name, distribution_file_name)
+    return send_from_directory("../..", file, mimetype='image/jpeg')
+
+
+@single_graph_api.route(
+    CONFIGURATIONS['endpoints']['proximity_click'] + '/<graph_name>/<float(signed=True):x>/<float(signed=True):y>')
+def get_closest_vertex(graph_name, x, y):
+    # return str(x) + str(y) + str(graph_name)
+    db_query_result = singletonNiceAbstraction.get_closest_point(x, y, VERTEX_TABLE_NAME(graph_name))
+    eth = db_query_result['eth'][0]
+    closest_point = wkt_to_x_y_list(db_query_result['st_astext'][0])
+    return {'eth': eth,
+            'x': closest_point[0],
+            'y': closest_point[1]}
