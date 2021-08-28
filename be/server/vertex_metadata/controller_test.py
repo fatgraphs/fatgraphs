0641b091810d121039f0b866139c515bc6d792e0
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-08-28 08:24:21 +0200

    added tests for vertex_metadata entity

---
+++ be/server/vertex_metadata/controller_test.py
@@ -0,0 +1,50 @@
+from random import random
+from unittest.mock import patch
+from flask.testing import FlaskClient
+
+from .service import  VertexMetadataService
+from .vertex_fixtures import vertex_metedata_1, vertex_metedata_2, vertex_metadata_1_param
+from ..test.fixtures import client, app  # noqa
+
+
+from . import BASE_ROUTE, VertexMetadata, VertexMetadataSchema
+
+
+class TestGraphResource:
+    @patch.object(
+        VertexMetadataService,
+        "get_by_eth",
+        lambda eth, db : [
+            vertex_metedata_1
+        ],
+    )
+    def test_get(self, client: FlaskClient):
+        with client:
+            results = client.get(f"/tokengallery/{BASE_ROUTE}/vertex/test_eth", follow_redirects=True).get_json()
+            expected = (
+                VertexMetadataSchema(many=True)
+                .dump(
+                    [
+                        vertex_metedata_1
+                    ]
+                )
+                
+            )
+            assert len(results) == 1
+            for r in results:
+                assert r in expected
+
+    @patch.object(
+                VertexMetadataService, "create", lambda metadata_to_insert, db: VertexMetadata(**metadata_to_insert)
+            )
+    def test_post(self, client: FlaskClient, vertex_metadata_1_param):  # noqa
+        with client:
+
+            result = client.post(f"/tokengallery/{BASE_ROUTE}/create", json=vertex_metadata_1_param).get_json()
+
+            expected = (
+                VertexMetadataSchema()
+                .dump(VertexMetadata(**vertex_metadata_1_param))
+
+            )
+            assert result == expected
---
+++ be/server/vertex_metadata/vertex_fixtures.py
@@ -0,0 +1,38 @@
+from copy import deepcopy
+import pytest
+from be.server.vertex_metadata import VertexMetadata
+from be.server.vertex_metadata.interface import VertexMetadataInterface
+
+
+@pytest.fixture
+def vertex_metadata_1_param():
+    yield dict(id=1,
+                eth='test_eth',
+                type='test_type',
+                label='test_label',
+                description='test_description')
+
+
+@pytest.fixture
+def vertex_metedata_1(vertex_metadata_1_param: VertexMetadataInterface):
+    yield VertexMetadata(**vertex_metadata_1_param)
+
+@pytest.fixture
+def vertex_metedata_1_different_id(vertex_metadata_1_param: VertexMetadataInterface):
+    new_params = deepcopy(vertex_metadata_1_param)
+    new_params['id'] = 123456789
+    yield VertexMetadata(**new_params)
+
+
+@pytest.fixture
+def vertex_metadata_2_param():
+    yield dict(id=2,
+                eth='test_eth_2',
+                type='test_type_2',
+                label='test_label_2',
+                description='test_description_2')
+
+
+@pytest.fixture
+def vertex_metedata_2(vertex_metadata_2_param: VertexMetadataInterface):
+    yield VertexMetadata(**vertex_metadata_2_param)
