b99958b574cb666f9ecf4f0088ae5640c7cc52a4
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-08-28 10:24:06 +0200

    added test

--- be/server/graph/service.py
+++ be/server/graph/service.py
@@ -50,3 +50,8 @@ class GraphService:
         update = graph.update(changes)
         db.commit()
         return update
+
+    @staticmethod
+    def get_vertex_table_name(graph_id: int, db) -> str:
+        graph = GraphService.get_by_id(graph_id)
+        return graph.graph_name + '_' + graph.id
