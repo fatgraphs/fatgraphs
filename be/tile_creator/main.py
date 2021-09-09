b85b46ae900da37f90bf7fd53eab53d984d999c4
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-09-09 22:31:19 +0200

    generate icons script to transform a raw png into the labelled ca and eoa versions

--- be/tile_creator/main.py
+++ be/tile_creator/main.py
@@ -10,6 +10,7 @@ from be.tile_creator.src.graph.gt_token_graph import GraphToolTokenGraph
 from be.tile_creator.src.graph.token_graph import TokenGraph
 from be.tile_creator.src.layout.visual_layout import VisualLayout
 from be.tile_creator.src.metadata.token_graph_metadata import TokenGraphMetadata
+from be.tile_creator.src.render.edge_distribution_plot_renderer import EdgeDistributionPlotRenderer
 from be.tile_creator.src.render.tiles_renderer import TilesRenderer
 from be.tile_creator.src.render.transparency_calculator import TransparencyCalculator
 from be.utils import make_geoframe
@@ -85,9 +86,9 @@ def main(configurations):
     tilesRenderer = TilesRenderer(gtGraph, metadata, configurations)
     tilesRenderer.renderGraph()
 
-    # print("rendering edge distributions plots...")
-    # edgePlotsRenderer = EdgeDistributionPlotRenderer(configurations, visualLayout)
-    # edgePlotsRenderer.render()
+    print("rendering edge distributions plots...")
+    edgePlotsRenderer = EdgeDistributionPlotRenderer(configurations, visualLayout)
+    edgePlotsRenderer.render()
 
 
 
