0a80002b6cb021ffa6f381755e2df7b54a350fe0
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-09-18 09:46:55 +0200

    scrollbar on edge transparency plots

--- be/server/server.py
+++ be/server/server.py
@@ -1,7 +1,9 @@
 import os
 
 from be.server import create_app
-
+if os.getcwd().split(os.sep)[-1] == "commands":
+    # if it's run from the commands frolder then set woring dir to be root
+    os.chdir(os.path.abspath(".."))
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
     app.run(debug=True)
