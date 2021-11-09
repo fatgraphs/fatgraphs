661c49eb7ab036e4d165359d5ca5ddb72e5395a1
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-11-09 21:21:21 +0100

    graphs accessible via the flask admin plugin

--- be/server/graph/__init__.py
+++ be/server/graph/__init__.py
@@ -1,5 +1,8 @@
+from flask_admin.contrib.sqla import ModelView
+
 from .model import Graph  # noqa
 from .schema import GraphSchema  # noqa
+from .. import admin, SessionLocal
 
 BASE_ROUTE = "graph"
 
@@ -8,3 +11,7 @@ def register_routes(api, app):
     from .controller import api as user_api
     root = app.config['API_ROOT']
     api.add_namespace(user_api, path=f"/{root}/{BASE_ROUTE}")
+    admin.add_view(ModelView(Graph, SessionLocal()))
+
+# make the gallery categories accessible via the flask admin interface
+
