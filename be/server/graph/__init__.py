bcfd6c055b51537d53362b6c3fdd1cf44d144dcf
segatcarlo95@gmail.com <segatcarlo95@gmail.com>
2021-11-10 14:22:39 +0100

    deleting a graph from the admin interface also deletes the vertex and edge table and the tile folder

--- be/server/graph/__init__.py
+++ be/server/graph/__init__.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from flask import flash
 from flask_admin.babel import gettext
 from flask_admin.contrib.sqla import ModelView
@@ -6,7 +9,7 @@ from psycopg2._psycopg import AsIs
 from .model import Graph  # noqa
 from .schema import GraphSchema  # noqa
 from .. import admin, SessionLocal
-from ...configuration import VERTEX_TABLE_NAME, EDGE_TABLE_NAME
+from ...configuration import VERTEX_TABLE_NAME, EDGE_TABLE_NAME, CONFIGURATIONS, TILE_FOLDER_NAME
 
 BASE_ROUTE = "graph"
 
@@ -22,41 +25,44 @@ def register_routes(api, app):
         column_hide_backrefs = False
         column_list = ('id', 'graph_name', 'vertices', 'edges', 'graph_category')
 
-        def delete_model(self, model):
+        def delete_model(self, graph):
 
             try:
+                try:
 
-                delte_vertex_table = """DROP TABLE %(table_name)s;"""
+                    delte_vertex_table = """DROP TABLE %(table_name)s;"""
+                    result = self.session.bind.engine.execute(delte_vertex_table, {
+                        'table_name': AsIs(VERTEX_TABLE_NAME(graph.id)),
+                    })
 
-                result = self.session.bind.engine.execute(delte_vertex_table, {
-                    'table_name': AsIs(VERTEX_TABLE_NAME(model.id)),
-                })
+                    delte_edge_table = """DROP TABLE %(table_name)s;"""
+                    result = self.session.bind.engine.execute(delte_edge_table, {
+                        'table_name': AsIs(EDGE_TABLE_NAME(graph.id)),
+                    })
 
-                delte_edge_table = """DROP TABLE %(table_name)s;"""
+                    delete_configuration_entry = """DELETE FROM tg_graph_configs WHERE tg_graph_configs.graph = %(graph_id)s;"""
+                    result = self.session.bind.engine.execute(delete_configuration_entry, {
+                        'graph_id': AsIs(graph.id),
+                    })
+                except Exception as e:
+                    self.session.rollback()
+                    return False
 
-                result = self.session.bind.engine.execute(delte_edge_table, {
-                    'table_name': AsIs(EDGE_TABLE_NAME(model.id)),
-                })
-
-                delete_configuration_entry = """DELETE FROM tg_graph_configs WHERE tg_graph_configs.graph = %(graph_id)s;"""
-
-                result = self.session.bind.engine.execute(delete_configuration_entry, {
-                    'graph_id': model.id,
-                })
-
-                self.on_model_delete(model)
+                self.on_model_delete(graph)
                 self.session.flush()
-                self.session.delete(model)
+                self.session.delete(graph)
                 self.session.commit()
+
+                shutil.rmtree(os.path.join(CONFIGURATIONS['graphsHome'], TILE_FOLDER_NAME(graph.id)))
+
             except Exception as ex:
                 if not self.handle_view_exception(ex):
                     flash(gettext('Failed to delete record. %(error)s', error=str(ex)), 'error')
-
                 self.session.rollback()
 
                 return False
             else:
-                self.after_model_delete(model)
+                self.after_model_delete(graph)
 
             return True
 
