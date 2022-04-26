ac6eb3d0c04c0de6ea7bdce9178d6db07dcf5568
Friedhelm Victor <friedhelm.victor@tu-berlin.de>
2022-04-26 13:24:02 +0200

    add about page

--- fe/map_client/src/components/about/About.js
+++ fe/map_client/src/components/about/About.js
@@ -4,8 +4,16 @@ class About extends Component {
 
     render() {
         return (
-            <div>
-                <p>todo: write up information about token gallery</p>
+            <div class="about">
+                <h1>About</h1>
+                <p>This is a visualization tool that allows for the interactive exploration of potentially large graphs. It is still work in progress, and was built by Carlo Segat and Friedhelm Victor</p>
+                <p>It makes use of the following technologies:</p>
+                <ul>
+                    <li>React.js</li>
+                    <li>Leaflet maps</li>
+                    <li>CuGraph</li>
+                </ul>
+                <p>It is not publically released yet, but will soon be available on GitHub. We will point this subdomain the corresponding repository in the future.</p>
             </div>
         );
     }
