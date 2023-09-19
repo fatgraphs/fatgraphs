6d1dff0fe5e83c1b53226e4ab8b56dcfeb1b03ff
CarloSegat <segatcarlo95@gmail.com>
2023-09-19 13:34:27 +0200

    refactor(main gtm code)
    The main part of the GTM code now use all the stuff
    that I build: API layer to make calls to the server
    
    The shape generator is not needed anymore because
    aggregation of metadata about vertices is done server
    side and the code to calculate the shapes client-side
    was simplified

--- be/tile_creator_2/main.py
+++ be/tile_creator_2/main.py
@@ -1,29 +1,21 @@
+import json
 import os
-
+import cudf
+import pandas as pd
 import geopandas as gpd
-import requests
 from geoalchemy2 import (
-    Geometry,
     WKTElement,
 )
 
 from be.configuration import (
-    EDGE_TABLE_NAME,
     SRID,
     TILE_FOLDER_NAME,
-    VERTEX_TABLE_NAME,
     data_folder,
     external_id,
     graphs_home,
     internal_id,
 )
-from be.server import SessionLocal
-from be.server.edge.service import EdgeService
-from be.server.graph.interface import GraphInterface
-from be.server.graph.service import GraphService
-from be.server.graph_configuration.interface import GraphConfigurationInterface
-from be.server.graph_configuration.service import GraphConfigurationService
-from be.server.vertex.service import VertexService
+
 from be.tile_creator_2.api.api import ApiLayer
 from be.tile_creator_2.cudf_graph import CudfGraph
 from be.tile_creator_2.datasource import DataSource
@@ -32,7 +24,6 @@ from be.tile_creator_2.edge_transparency_plots import EdgeTransparencyPlots
 from be.tile_creator_2.graph_data import GraphData
 from be.tile_creator_2.graph_tool_token_graph import GraphToolTokenGraph
 from be.tile_creator_2.gtm_args import GtmArgs
-from be.tile_creator_2.shape_calculator import ShapeGenerator
 from be.tile_creator_2.tiles_renderer import TilesRenderer
 from be.tile_creator_2.transparency_calculator import TransparencyCalculator
 from be.tile_creator_2.vertex_data import VertexData
@@ -82,82 +73,102 @@ def main(configurations):
     graph_data.set_edge_count(edge_data)
     graph_data.set_vertex_count(vertex_data)
     graph_data.set_description(gtm_args.get_description())
-
-    with SessionLocal() as db:
         
-        
-        persisted_graph = ApiLayer.graph.post(graph_data.to_json_camelcase())
-        # persisted_graph = GraphService.create(GraphInterface.from_graph_data(graph_data), db)
+    persisted_graph = ApiLayer.graph.post(graph_data.to_json_camelcase())
 
-        graph_data.set_bounding_square(vertex_data)
-        graph_data.set_bounding_square_pixel(gtm_args)
+    graph_data.set_bounding_square(vertex_data)
+    graph_data.set_bounding_square_pixel(gtm_args)
 
-        vertex_data.set_positions_pixel(gtm_args, graph_data)
+    vertex_data.set_positions_pixel(gtm_args, graph_data)
 
-        vertex_data.set_corner_vertex_positions(graph_data)
+    vertex_data.set_corner_vertex_positions(graph_data)
 
-        edge_data.set_ids_to_position(vertex_data)
-        edge_data.set_ids_to_pixel_position(vertex_data)
+    edge_data.set_ids_to_position(vertex_data)
+    edge_data.set_ids_to_pixel_position(vertex_data)
 
-        graph_data.set_median_pixel_distance(vertex_data)
+    graph_data.set_median_pixel_distance(vertex_data)
 
-        vertex_data.set_sizes(graph_data, gtm_args)
+    vertex_data.set_sizes(graph_data, gtm_args)
 
-        edge_data.set_thickness(graph_data, gtm_args)
-        edge_data.set_lengths()
+    edge_data.set_thickness(graph_data, gtm_args)
+    edge_data.set_lengths()
 
-        # TODO maybe move in
-        tc = TransparencyCalculator(graph_data, gtm_args)
-        transparencies = tc.calculateEdgeTransparencies(edge_data.get_lengths().to_numpy())
-        edge_data.transparencies = transparencies
+    tc = TransparencyCalculator(graph_data, gtm_args)
+    transparencies = tc.calculateEdgeTransparencies(edge_data.get_lengths().to_numpy())
+    edge_data.transparencies = transparencies
 
-        # save the configuration to db
-        # TODO save bound in graph_data: need to change FE code!
+    # save the configuration to db
+    # TODO save bound in graph_data: need to change FE code!
+    config_obj = gtm_args.to_json_camel_case(graph_data, persisted_graph['id'])
+    ApiLayer.configs.post(config_obj)
 
-        config_obj = gtm_args.to_json_camel_case(graph_data, persisted_graph['id'])
-        
-        config_db = ApiLayer.configs.post(config_obj)
+    df_edges = vertex_data.cudf_frame.to_pandas()
+    df_edges['graph_id'] = persisted_graph['id']
+    geo_frame = make_geoframe(df_edges)
 
-        # save vertices to db
-        vertex_table = VERTEX_TABLE_NAME(persisted_graph['id'])
-        VertexService.ensure_vertex_table_exists(vertex_table, persisted_graph['id'])
+    ApiLayer.vertices.post_stream(
+        geo_frame[['graph_id', 'vertex', 'size', 'pos']]
+    )
 
-        f = vertex_data.cudf_frame.to_pandas()
-        f['graph_id'] = persisted_graph['id']
-        geo_frame = make_geoframe(f)
-        print("geo_frame", geo_frame[['graph_id', 'vertex', 'size', 'pos']])
-        geo_frame[['graph_id', 'vertex', 'size', 'pos']].to_sql(
-            vertex_table,
-            db.bind.engine,
-            if_exists='append',
-            index=False,
-            dtype={'pos': Geometry('POINT', srid=SRID)})
+    # fetch all the vertices metadata
+    res = ApiLayer.vertex_metadata.get_all_for_graph(persisted_graph['id'])
 
-        shg = ShapeGenerator(db, persisted_graph['id'])
-        vertex_data = shg.augment_vertex_data(vertex_data)
+    int_to_icon = {
+            0: 'inactive_fake',
+            1: 'eoa_unlabelled',
+            2: 'eoa_labelled',
+            3: 'ca_unlabelled',
+            4: 'ca_labelled'
+        }
+    
+    # TODO make it so after N row it merges them to the frame
+    # iin this way we won't trash  memory. Right now we are loading into 
+    # memory everything 
+    vertex_metadata_subset = []
+    for row in res.iter_lines(chunk_size=1):
+        row = json.loads(row.decode())
 
-        # save edges to db
-        edge_table = EDGE_TABLE_NAME(persisted_graph['id'])
-        EdgeService.ensure_edge_table_exists(edge_table, persisted_graph['id'])
+        code = (row['type'] * 2) + 1
+        code += 1 if row['label'] else 0
+        code = row['icon'] if row['icon'] is not None else int_to_icon[code]
 
-        f = edge_data.cudf_frame.to_pandas()
-        f['graph_id'] = persisted_graph['id']
-        f = f.rename(columns={external_id('source'): 'src',
-                              external_id('target'): 'trg'})
-        f[['src', 'trg', 'amount', 'graph_id']].to_sql(edge_table,
-                                                       db.bind.engine,
-                                                       if_exists='append', index=False)
+        vertex_metadata_subset.append(
+            dict(vertex=row['vertex'], code=code)
+        )
 
-        gt_graph = make_gt_graph(edge_data, gtm_args, vertex_data)
+    vertex_metadata_subset = pd.DataFrame.from_records(vertex_metadata_subset)
+    vertex_metadata_subset = cudf.DataFrame.from_pandas(vertex_metadata_subset)
 
-        # as soon as we have the id we can make the graph folder
-        output_folder = mkdir_for_graph(persisted_graph['id'])
+    vertex_df_augmented = vertex_data.cudf_frame.merge(
+        vertex_metadata_subset, 
+        on='vertex', 
+        how='left'
+    )
 
-        plotter = EdgeTransparencyPlots(graph_data, gtm_args, edge_data)
-        plotter.render(output_folder)
+    vertex_df_augmented.fillna('inactive_fake', inplace=True)
 
-        tr = TilesRenderer(gt_graph, output_folder, graph_data.get_graph_bound(), graph_data.get_pixel_bound())
-        tr.render_graph()
+    vertex_data.cudf_frame = vertex_df_augmented
+
+    # save edges to db
+    df_edges = edge_data.cudf_frame.to_pandas()
+    df_edges['graph_id'] = persisted_graph['id']
+    df_edges = df_edges.rename(columns={external_id('source'): 'src',
+                            external_id('target'): 'trg'})
+    
+    ApiLayer.edges.post_stream(
+        df_edges[['src', 'trg', 'amount', 'graph_id']]
+    )
+
+    gt_graph = make_gt_graph(edge_data, gtm_args, vertex_data)
+
+    # as soon as we have the id we can make the graph folder
+    output_folder = mkdir_for_graph(persisted_graph['id'])
+
+    plotter = EdgeTransparencyPlots(graph_data, gtm_args, edge_data)
+    plotter.render(output_folder)
+
+    tr = TilesRenderer(gt_graph, output_folder, graph_data.get_graph_bound(), graph_data.get_pixel_bound())
+    tr.render_graph()
 
 @timeit("Constructing the graph_tool graph")
 def make_gt_graph(edge_data, gtm_args, vertex_data):
