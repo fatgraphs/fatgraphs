3f7edb154bd40d016e1ab0ea6395a553e33786c4
CarloSegat <segatcarlo95@gmail.com>
2023-10-14 08:47:45 +0200

    refactor
    add more data during initialisation (basically for development)
    
    reordered how tables are created to fix somehting

--- commands/scripts/load_account_types.sql
+++ commands/scripts/load_account_types.sql
@@ -12,6 +12,3 @@ BEGIN;
 
 COMMIT;
 
-insert into tg_account_type (vertex, type) VALUES ('0xe9c1a41b0ba27e80b138c0e17e7cc681b26099cf', 1);
-insert into tg_account_type (vertex, type) VALUES ('0xef51c9377feb29856e61625caf9390bd0b67ea18', 1);
-insert into tg_account_type (vertex, type) VALUES ('0x997c48ce1af0ce2658d3e4c0bea30a0eb9c98382', 1);
